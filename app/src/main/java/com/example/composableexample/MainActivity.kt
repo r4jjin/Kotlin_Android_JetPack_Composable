4a94b32c87084384a4437937835ba7967e96390d
Detoxic Spirit <r4jjin@gmail.com>
2021-06-09 19:47:31 +0530

    Update MainActivity.kt
    ignore

--- app/src/main/java/com/example/composableexample/MainActivity.kt
+++ app/src/main/java/com/example/composableexample/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            
             Column(
                 modifier = Modifier
 //                    .fillMaxSize(0.5f)
