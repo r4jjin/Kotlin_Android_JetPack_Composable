af76f7f1adf1624cde5b7d4b7288eb66d4a5a2ae
Detoxic Spirit <r4jjin@gmail.com>
2021-08-02 08:22:32 +0530

    preview function using @Preview

--- app/src/main/java/com/example/composableexample/MainActivity.kt
+++ app/src/main/java/com/example/composableexample/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +18,11 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun MessageText(text: String) {
         Text(text = "Intro : $text")
+    }
+
+    @Preview
+    @Composable
+    fun Preview() {
+        MessageText("Hi")
     }
 }
