82aac28be739e53842cae751d34d94f12d801c9c
Detoxic Spirit <r4jjin@gmail.com>
2021-08-02 09:40:01 +0530

    configure layout using modifiers

--- app/src/main/java/com/example/composableexample/MainActivity.kt
+++ app/src/main/java/com/example/composableexample/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +27,21 @@ data class Message(val name: String, val details: String)
 
 @Composable
 fun MessageText(text: Message) {
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "profile pic."
+            contentDescription = "profile pic.",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
         )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column {
             Text(text = "Name : ${text.name}")
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = "Description : ${text.details}")
         }
     }
