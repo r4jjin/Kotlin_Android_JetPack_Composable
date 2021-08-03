999bb4ccfd986c246a7ce1316440d1be1ed16d21
Detoxic Spirit <r4jjin@gmail.com>
2021-08-03 05:34:42 +0530

    applying material design color, typography

--- app/src/main/java/com/example/composableexample/MainActivity.kt
+++ app/src/main/java/com/example/composableexample/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,15 +36,25 @@ fun MessageText(text: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         // Add a horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = "Name : ${text.name}")
+            Text(
+                text = "Name : ${text.name}",
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+
+            )
             // Add a vertical space between the author and message texts
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = "Description : ${text.details}")
+            Text(
+                text = "Description : ${text.details}",
+                style = MaterialTheme.typography.body2
+
+            )
         }
     }
 }
